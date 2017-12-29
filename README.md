25bed6a154f2c16f44aa693544d0b9656144a04e
IncredibleQuark <lkupinski@gmail.com>
2017-12-29 22:26:03 +0100

    returning data and display

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # TwitchTvApi
+
+It is an easy script to check if particular streams from twitch.tv are online, made for FCC project.
--- app.js
+++ app.js
@@ -2,20 +2,70 @@
 
 $( document ).ready(function() {
 
+let streams = ['blackfireice', 'freecodecamp'];
 
-    function callTwich() {
+
+streams.forEach(function (stream) {
+
+    callTwitch(stream);
+});
+
+
+//to make this fnc work you need to allow CORS
+    function callTwitch(stream) {
         $.ajax({
+            mode: 'cors',
             type: 'GET',
-            url: 'https://api.twitch.tv/kraken/users/quarkincredible?client_id=jvomfm0gddh7dfs2vzd8yzcy3q80uk',
+            url: 'https://wind-bow.gomix.me/twitch-api/users/' + stream,
             dataType: 'json'
 
         }).done((data) => {
-            console.log(data);
+console.warn(data);
+            const link = $('<a>'+data.display_name+'</a>');
+            const listItem = $('<li class="list-group-item"></li>');
+
+            if (data) {
+
+                $.ajax({
+                    mode: 'cors',
+                    type: 'GET',
+                    url: 'https://wind-bow.gomix.me/twitch-api/streams/'+stream,
+                    dataType: 'json'
+
+                }).done((data1) => {
+
+
+                    console.log(data1);
+
+                    if(data1.stream) {
+
+                        link.attr('href', data1.stream.channel.url);
+                        const img = $('<img alt="preview" class="right" src='+data1.stream.preview.medium+'/>');
+                        img.appendTo(listItem);
+
+
+                        return true;
+                    } else {
+                        return false;
+                    }
+
+                }).fail((err) => {
+                    console.error('Something went wrong!');
+                    console.log(err);
+                    return false;
+                });
+
+            }
+            listItem.appendTo('#streamers');
+            link.appendTo(listItem);
+
+
         }).fail((err) => {
+            console.error('Something went wrong!');
             console.log(err);
+            return false;
         });
     }
 
-    callTwich();
 
 });
